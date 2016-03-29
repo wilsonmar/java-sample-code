1aa29a7739a8cba979b8847bf2588454ea24b287
Wilson Mar <wilsonmar@gmail.com>
2016-03-29 10:17:44 -0600

    Update java sample with javc_all

--- HelloWorld1.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-
-    // method main(): ALWAYS the APPLICATION entry point
-    public static void main (String[] args) {
-	System.out.println ("Hello World!");
-    }
-}
---
+++ javac_all.sh
@@ -0,0 +1,36 @@
+# javac_all.sh
+# Compile all java programs:
+# Before running: chmod a+x javac_all.sh
+
+#
+javac ArrayDemo.java
+#
+javac DivBy0.java
+#
+javac FunctionCall.java
+#
+javac HelloDate.java
+#
+javac HelloWorld.class
+#
+javac HelloWorld.java
+#
+javac HelloWorld1.java
+#
+javac HelloWorldException.java
+#
+javac KeyboardIntegerReader.java
+#
+javac KeyboardReader.java
+#
+javac KeyboardReaderError.java
+#
+javac MyFileReader.java
+#
+javac MyFileWriter.java
+#
+javac Point2d.java
+#
+javac Point3d.java
+#
+javac PointTester.java
